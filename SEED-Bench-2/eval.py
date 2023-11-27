1a287170000107c40c61e87bb063f1e32ec13535
Bohao-Lee <74533144+Bohao-Lee@users.noreply.github.com>
2023-11-27 16:36:17 +0800

    Add files via upload

---
+++ SEED-Bench-2/eval.py
@@ -0,0 +1,135 @@
+import os
+import json
+import argparse
+
+import torch
+from tqdm import tqdm
+import numpy as np
+import random
+import pdb
+
+# root directory of cc3m
+cc3m_dir = "/YOUR_PATH_TO/seed_bench_image"
+# root directory of seed bench v2
+seed_bench_v2_dir = "/YOUR_PATH_TO/seed_bench_image_v2"
+
+seed = 0
+
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+def filter_questions(data, level='L2', subpart='all', version='v2'):
+    if level == "L1":
+        valid_level_data = ['L1']
+    elif level == "L2":
+        valid_level_data = ['L1', 'L2']
+    elif task == "L3":
+        valid_level_data = ['L1', 'L2', 'L3']
+    else:
+        raise ValueError(f"Invalid level: {level}")
+    data = [q for q in data if q["level"] in valid_level_data]
+
+    if subpart in ['Single-Image & Text Comprehension', 'Multiple-Images & Text Comprehension', 'Video & Text Comprehension', 'Interleaved Image & Text Comprehension', 'Image Generation', 'Image & Text Generation']:
+        valid_subgroup_data = subgroup
+    elif subpart == 'all':
+        valid_subgroup_data = ['Single-Image & Text Comprehension', 'Multiple-Images & Text Comprehension', 'Video & Text Comprehension', 'Interleaved Image & Text Comprehension', 'Image Generation', 'Image & Text Generation']
+    else:
+        raise ValueError(f"Invalid subpart: {subpart}")
+    data = [q for q in data if q["subpart"] in valid_subgroup_data]
+
+    if version == 'v1':
+        valid_version_data = ['v1']
+    elif version == 'v2':
+        valid_version_data = ['v1', 'v2']
+    else:
+        raise ValueError(f"Invalid version: {version}")
+    data = [q for q in data if q["version"] in valid_version_data]
+
+    return data
+
+def build_model(model_name):
+    if model_name == 'InternLM_Xcomposer_VL':
+        from model.InternLM_Xcomposer_VL_interface import build
+    model = build()
+
+    return model
+
+def is_integer_string(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def run_inference(model, qa_anno, output_dir):
+    total_qa_num = len(qa_anno)
+    answer_list = []
+    output_f = open(os.path.join(output_dir, "results.json"), "a")
+    step = 0
+    for qa_item in tqdm(qa_anno):
+
+        data_info = {
+            'question': qa_item['question'],
+            'choices': [qa_item['choice_a'], qa_item['choice_b'], qa_item['choice_c'], qa_item['choice_d']],
+        }
+
+        if qa_item["data_source"] == 'cc3m':
+            image_dir = cc3m_dir
+        elif qa_item["data_source"] == 'SEED-Bench v2':
+            image_dir = seed_bench_v2_dir
+        else:
+            raise ValueError("The data type is not valid.")
+        if type(qa_item['data_id']) is list:
+            data_path = [os.path.join(image_dir, path) for path in qa_item['data_id']]
+        else:
+            data_path = os.path.join(image_dir, qa_item['data_id'])
+        data_info['data_path'] = data_path
+
+        # losses: loss values of 4 choices, shape=[4]
+        with torch.no_grad():
+            losses = model(data_info)
+        class_ranks = np.argsort(losses)
+        pred_id = ['A', 'B', 'C', 'D'][class_ranks[0]]
+        gt = qa_item['answer']
+        answer_record = {
+            'question_id': qa_item['question_id'],
+            'prediction': pred_id
+        }
+        answer_list.append(answer_record)
+        # output prediction record for each question
+        output_f.write(json.dumps(answer_record) + "\n")
+        step += 1
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Arg Parser')
+    parser.add_argument('--model', type=str, default='InternLM_Xcomposer_VL')
+    parser.add_argument('--anno_path', type=str, default='SEED-Bench_v2_level1_2_3.json')
+    parser.add_argument('--output_dir', type=str, default='results')
+    parser.add_argument('--evaluate_level', type=str, default='L2')
+    parser.add_argument('--evaluate_part', type=str, default='all')
+    parser.add_argument('--evaluate_version', type=str, default='v2')
+    args = parser.parse_args()
+    
+    args = parser.parse_args()
+
+    qa_anno = json.load(open(args.anno_path, 'rb'))
+    if 'questions' in qa_anno.keys():
+        qa_anno = qa_anno['questions']
+    qa_anno = filter_questions(qa_anno, args.evaluate_level, args.evaluate_part, args.evaluate_version)
+    pdb.set_trace()
+
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+
+    print(f'evaluating.. {args.model}')
+    # The interface for testing MLLMs
+    model = build_model(args.model).cuda()
+    run_inference(model, qa_anno, args.output_dir)
---
+++ SEED-Bench-2/model/InternLM_Xcomposer_VL_interface.py
@@ -0,0 +1,227 @@
+import os
+import os.path as osp
+import sys
+import json
+from PIL import Image
+import torch
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.generation import GenerationConfig
+
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from torchvision.transforms.functional import InterpolationMode
+import pdb
+import numpy as np
+
+def normalize():
+    mean = (0.48145466, 0.4578275, 0.40821073)
+    std = (0.26862954, 0.26130258, 0.27577711)
+    return transforms.Normalize(mean, std)
+
+class MLLM_Tester(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # initialization of InternLM_Xcomposer_VL
+        InternLM_Xcomposer_path = "./InternLM_Xcomposer_VL_weight_path"
+        self.model = AutoModelForCausalLM.from_pretrained(
+            InternLM_Xcomposer_path, device_map='cuda', trust_remote_code=True).eval()
+        
+        self.tokenizer = AutoTokenizer.from_pretrained(InternLM_Xcomposer_path,
+                                                trust_remote_code=True)
+        for n, p in self.model.named_parameters():
+            p.requires_grad = False
+
+        self.model.tokenizer = self.tokenizer
+        self.vis_processor = transforms.Compose(
+            [
+                transforms.Resize(
+                    (224, 224), interpolation=InterpolationMode.BICUBIC
+                ),
+                transforms.ToTensor(),
+                normalize(),
+            ]
+        )
+
+    def process_qa(self, question, candidates):
+        options = candidates
+        options_prompt = f'A. {options[0]}\nB. {options[1]}\nC. {options[2]}\nD. {options[3]}\n'  # noqa
+
+        img_prompt = ' <|User|>:<ImageHere>'
+        context = 'N/A'
+        
+        mid_prompt = 'Context: ' + context + '\nQuestion: ' + question + '\nOptions: []\n' + options_prompt
+        ans_prompt = ' <|Bot|>: Answer: The answer is'
+        text = img_prompt + mid_prompt + '<TOKENS_UNUSED_0>' + ans_prompt
+
+        return text
+
+    def process_interleved_qa(self, question, candidates):
+        options = candidates
+        options_prompt = f'A. {options[0]}\nB. {options[1]}\nC. {options[2]}\nD. {options[3]}\n'  # noqa
+
+        img_prompt = ' <|User|>:'
+        context = 'N/A'
+        question = question.replace("[img]", '<ImageHere>')
+        
+        mid_prompt = 'Context: ' + context + '\nQuestion: ' + question + '\nOptions: []\n' + options_prompt
+        ans_prompt = ' <|Bot|>: Answer: The answer is'
+        text = img_prompt + mid_prompt + '<TOKENS_UNUSED_0>' + ans_prompt
+
+        return text
+
+    def generate_answer_with_ppl(self, base_prompt, image):
+        choice_mapping = ['A.', 'B.', 'C.', 'D.']
+        if type(image) is list:
+            image_embeds = []
+            for current_image in image:
+                current_image_embed = self.model.encode_img(current_image)
+                image_embeds.append(current_image_embed)
+            img_embeds = torch.stack(image_embeds, dim=1).squeeze(0)
+            N, L, C = img_embeds.shape
+            img_embeds = img_embeds.reshape(1, -1, C)
+        else:
+            img_embeds = self.model.encode_img(image)
+        prompt = base_prompt
+        prompt_segs = prompt.split('<ImageHere>')
+        prompt_seg_tokens = [
+            self.model.tokenizer(seg,
+                                return_tensors='pt',
+                                add_special_tokens=i == 0).
+            to(self.model.internlm_model.model.embed_tokens.weight.device).input_ids
+            for i, seg in enumerate(prompt_segs)
+        ]
+        prompt_seg_embs = [
+            self.model.internlm_model.model.embed_tokens(seg)
+            for seg in prompt_seg_tokens
+        ]
+        prompt_seg_embs = [prompt_seg_embs[0], img_embeds, prompt_seg_embs[1]]
+        prompt_embs = torch.cat(prompt_seg_embs, dim=1)
+
+        im_targets = torch.ones(img_embeds.shape[0], img_embeds.shape[1], dtype=torch.long).to(img_embeds.device) * self.model.tokenizer.pad_token_id
+        tars = torch.cat([prompt_seg_tokens[0], im_targets, prompt_seg_tokens[1]], dim=1)
+        atts_mask = torch.ones(prompt_embs.size()[:-1], dtype=torch.long).to(self.model.device)
+
+        len_prompt = tars.shape[1] - 1
+        candis = choice_mapping
+        op_tokens = self.model.tokenizer(candis, return_tensors="pt", add_special_tokens=False, padding=True).to(self.model.device)
+        op_embeds = self.model.internlm_model.model.embed_tokens(op_tokens.input_ids)
+        tars = torch.cat([tars.repeat(4,1), op_tokens.input_ids], dim=1)
+        atts_mask = torch.cat([atts_mask.repeat(4,1), op_tokens.attention_mask], dim=1)
+        prompt_embs = torch.cat([prompt_embs.repeat(4,1,1), op_embeds], dim=1)
+        with torch.cuda.amp.autocast():
+            with torch.no_grad():
+                outputs = self.model.internlm_model(
+                    inputs_embeds=prompt_embs,
+                    attention_mask=atts_mask,
+                    return_dict=True,
+                    labels=None,
+                )
+        outputs = outputs.logits
+        shift_logits = outputs[..., len_prompt:-1, :].contiguous()
+        shift_labels = tars[..., 1+len_prompt:].contiguous()
+        shift_logits = shift_logits.view(-1, shift_logits.size(-1))
+        loss_fct = torch.nn.CrossEntropyLoss(
+            reduction='none', ignore_index=self.model.tokenizer.pad_token_id)
+        sf = torch.nn.functional.softmax(shift_logits, dim=-1)
+
+        loss = loss_fct(shift_logits, shift_labels.view(-1)).reshape(shift_labels.shape[0], shift_labels.shape[1])
+        mask_length = None
+
+        lens = (shift_labels !=
+                self.model.tokenizer.pad_token_id).sum(-1).cpu().numpy()
+        ce_loss = loss.sum(-1).cpu().detach().numpy() / lens
+
+        return ce_loss
+
+    def generate_interleved_answer_with_ppl(self, base_prompt, image):
+        choice_mapping = ['A.', 'B.', 'C.', 'D.']
+        if type(image) is list:
+            image_embeds = []
+            for current_image in image:
+                current_image_embed = self.model.encode_img(current_image)
+                image_embeds.append(current_image_embed)
+            img_embeds = torch.stack(image_embeds, dim=1).squeeze(0)
+            N, L, C = img_embeds.shape
+            # img_embeds = img_embeds.reshape(1, -1, C)
+        else:
+            img_embeds = self.model.encode_img(image)
+        prompt = base_prompt
+        prompt_segs = prompt.split('<ImageHere>')
+        prompt_seg_tokens = [
+            self.model.tokenizer(seg,
+                                return_tensors='pt',
+                                add_special_tokens=i == 0).
+            to(self.model.internlm_model.model.embed_tokens.weight.device).input_ids
+            for i, seg in enumerate(prompt_segs)
+        ]
+        prompt_seg_embs = [
+            self.model.internlm_model.model.embed_tokens(seg)
+            for seg in prompt_seg_tokens
+        ]
+        im_targets = torch.ones(img_embeds.shape[0], img_embeds.shape[1], dtype=torch.long).to(img_embeds.device) * self.model.tokenizer.pad_token_id
+        
+        embs = [prompt_seg_embs[0]]
+        tars = [prompt_seg_tokens[0]]
+        for index in range(img_embeds.shape[0]):
+            embs.append(img_embeds[index].unsqueeze(0))
+            embs.append(prompt_seg_embs[index + 1])
+            tars.append(im_targets[index].unsqueeze(0))
+            tars.append(prompt_seg_tokens[index + 1])
+
+        # prompt_seg_embs = [prompt_seg_embs[0], img_embeds, prompt_seg_embs[1]]
+        prompt_embs = torch.cat(embs, dim=1)
+        tars = torch.cat(tars, dim=1)
+        # tars = torch.cat([prompt_seg_tokens[0], im_targets, prompt_seg_tokens[1]], dim=1)
+        atts_mask = torch.ones(prompt_embs.size()[:-1], dtype=torch.long).to(self.model.device)
+
+        len_prompt = tars.shape[1] - 1
+        candis = choice_mapping
+        op_tokens = self.model.tokenizer(candis, return_tensors="pt", add_special_tokens=False, padding=True).to(self.model.device)
+        op_embeds = self.model.internlm_model.model.embed_tokens(op_tokens.input_ids)
+        tars = torch.cat([tars.repeat(4,1), op_tokens.input_ids], dim=1)
+        atts_mask = torch.cat([atts_mask.repeat(4,1), op_tokens.attention_mask], dim=1)
+        prompt_embs = torch.cat([prompt_embs.repeat(4,1,1), op_embeds], dim=1)
+        with torch.cuda.amp.autocast():
+            with torch.no_grad():
+                outputs = self.model.internlm_model(
+                    inputs_embeds=prompt_embs,
+                    attention_mask=atts_mask,
+                    return_dict=True,
+                    labels=None,
+                )
+        outputs = outputs.logits
+        shift_logits = outputs[..., len_prompt:-1, :].contiguous()
+        shift_labels = tars[..., 1+len_prompt:].contiguous()
+        shift_logits = shift_logits.view(-1, shift_logits.size(-1))
+        loss_fct = torch.nn.CrossEntropyLoss(
+            reduction='none', ignore_index=self.model.tokenizer.pad_token_id)
+        sf = torch.nn.functional.softmax(shift_logits, dim=-1)
+
+        loss = loss_fct(shift_logits, shift_labels.view(-1)).reshape(shift_labels.shape[0], shift_labels.shape[1])
+        mask_length = None
+
+        lens = (shift_labels !=
+                self.model.tokenizer.pad_token_id).sum(-1).cpu().numpy()
+        ce_loss = loss.sum(-1).cpu().detach().numpy() / lens
+
+        return ce_loss
+
+    def forward(
+        self,
+        x
+    ):
+        data_path, question, choices = x['data_path'], x['question'], x['choices']
+        if "[img]" not in question:
+            text = self.process_qa(question, choices)
+            result = self.generate_answer_with_ppl(text, data_path)
+        else: 
+            text = self.process_interleved_qa(question, choices)
+            result = self.generate_interleved_answer_with_ppl(text, data_path)
+
+        return result
+
+
+def build():
+    return MLLM_Tester()
